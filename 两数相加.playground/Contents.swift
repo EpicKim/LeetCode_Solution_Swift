66eae2c5a8ff274f0653f029c424f25612229181
超梦 <wangke02@bilibili.com>
2020-06-05 16:18:08 +0800

    update

--- 两数相加.playground/Contents.swift
+++ 两数相加.playground/Contents.swift
@@ -103,13 +103,38 @@ func getNumList(_ node:ListNode?, _ list:[Int]) -> [Int] {
 }
 
 class Solution {
+//    func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+//        let a1 = getNumList(l1, [])
+//        let a2 = getNumList(l2, [])
+//
+//        let a3 = addTwoArray(a1, a2)
+//
+//        return setupNode(nil, a3)
+//    }
+
     func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
-        let a1 = getNumList(l1, [])
-        let a2 = getNumList(l2, [])
-
-        let a3 = addTwoArray(a1, a2)
-
-        return setupNode(nil, a3)
+        var ll1 = l1 ,ll2 = l2 ,node = ListNode(0)
+        let nodes = node
+        var flag = 0
+        while (ll1?.val != nil || ll2?.val != nil || flag == 1)
+        {
+            let num1 : Int = ll1?.val ?? 0
+            let num2 : Int = ll2?.val ?? 0
+            var num = num1 + num2 + flag
+            if num > 9
+            {
+                num = num%10
+                flag = 1
+            }else
+            {
+                flag = 0
+            }
+            node.next = ListNode(num)
+            node = node.next!
+            ll1  = ll1?.next
+            ll2  = ll2?.next
+        }
+        return nodes.next
     }
 }
 
